05b3c0ec4d1de9b1ca51c5f2fd1526b80da139ba
eliza <twew02@gmail.com>
2022-11-05 17:10:53 -0400

    add get, post, delete and use route handler

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB");
 
-const articleSchema = new mongoose.Schema ({
+const articleSchema = new mongoose.Schema({
   title: String,
-  content: String
-})
+  content: String,
+});
 
-const Article = new mongoose.model("Article", articleSchema)
+const Article = new mongoose.model("Article", articleSchema);
+
+app.route("/articles")
+  .get(function (req, res) {
+    Article.find({}, function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const articleTitle = req.body.title;
+    const articleContent = req.body.content;
+
+    const newArticle = new Article({
+      title: articleTitle,
+      content: articleContent,
+    });
+    newArticle.save((err) => {
+      if (!err) {
+        res.send("Successfully added new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany({}, function (err) {
+      if (!err) {
+        res.send("All articles are deleted");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
 app.listen(3100, () => {
-  console.log("Server started on port 3100")
-})
+  console.log("Server started on port 3100");
+});
