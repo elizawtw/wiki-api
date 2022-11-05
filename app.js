fd5e481ea810dde479f0a99c5a888d7153f6f13d
eliza <twew02@gmail.com>
2022-11-05 14:30:41 -0400

    setup server

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = new mongoose.Schema ({
+  title: String,
+  content: String
+})
+
+const Article = new mongoose.model("Article", articleSchema)
+
 app.listen(3100, () => {
   console.log("Server started on port 3100")
 })
