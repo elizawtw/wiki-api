bc053ecc18f1b91bb592b48c4c0f82f3e04ae208
eliza <twew02@gmail.com>
2022-11-06 11:39:49 -0500

    add delete route

--- app.js
+++ app.js
@@ -91,6 +91,15 @@ app.route("/articles")
       }
     )
   })
+  .delete(function(req,res){
+    Article.deleteOne({title:req.params.articleTitle}, function(err){
+      if(!err){
+        res.send("Successfully deleted")
+      } else {
+        res.send(err)
+      }
+    })
+  })
 
 app.listen(3100, () => {
   console.log("Server started on port 3100");
